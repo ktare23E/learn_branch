fba8c62f9a287d2796e6cb752717ddeab2d499ee
Kristian <kristiankharl.tare@nmsc.edu.ph>
2025-06-21 20:18:06 +0800

    testing merge

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/testing',function(){
+    $data = "This is data";
+    return view('testing',[
+        'data' => $data
+    ]);
+});
